91cd129edec55b3072c9ddf42d9789ece96faf18
DallOrto <rafaeldallorto@gmail.comm>
2022-07-12 14:50:55 -0300

    feat: start project

---
+++ prisma/migrations/20220712172509_create_network/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "networks" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "networks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "schools" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "network_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "schools_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teachers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "birth_date" TEXT NOT NULL,
+    "school_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "teachers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "students" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "birth_date" TEXT NOT NULL,
+    "school_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "students_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "classes" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "class_day" TEXT NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "classes_pkey" PRIMARY KEY ("id")
+);
