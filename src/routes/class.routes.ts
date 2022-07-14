2d30dc5b2e5abd771f67e16d4895a901e00b1afb
DallOrto <rafaeldallorto@gmail.comm>
2022-07-13 21:47:59 -0300

    feat: add flow of relations - teacherClass

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { CreateClassController } from "../controllers/CreateClassController";
+import { CreateTeacherClassController } from "../controllers/CreateTeacherClassController";
 
 
 
 const createClassController = new CreateClassController();
+const createTeacherClassController = new CreateTeacherClassController();
+
 
 const classRoutes = Router();
 
 classRoutes.post("/", createClassController.handle);
+classRoutes.post("/teacher", createTeacherClassController.handle)
 
 export { classRoutes };
