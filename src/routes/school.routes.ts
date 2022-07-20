a8e22d7426cf9173ce02ae3d0ce05a5985df3a53
DallOrto <rafaeldallorto@gmail.comm>
2022-07-19 22:44:41 -0300

    feat: add flow of update school

--- src/routes/school.routes.ts
+++ src/routes/school.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { UpdateSchoolController } from "../controllers/network/UpdateSchoolController";
 import { CreateSchoolController } from "../controllers/school/CreateSchoolController";
 import { ListSchoolController } from "../controllers/school/ListSchoolController";
 
@@ -6,10 +7,12 @@ import { ListSchoolController } from "../controllers/school/ListSchoolController
 
 const createSchoolController = new CreateSchoolController();
 const listSchoolController = new ListSchoolController();
+const updateSchoolController = new UpdateSchoolController();
 
 const schoolRoutes = Router();
 
 schoolRoutes.post("/", createSchoolController.handle);
 schoolRoutes.get("/list", listSchoolController.handle);
+schoolRoutes.put("/:id", updateSchoolController.handle);
 
 export { schoolRoutes };
